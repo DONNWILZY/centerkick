0d5593c0beccdd542a22c34f61fef0f0a8df5b96
donnwilzy <ask4godswill@gmail.com>
2023-10-10 22:42:19 +0100

    progress

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+app.use(express.json());
+
+//// dot env 
+const dotenv = require('dotenv');
+dotenv.config()
+
+//// port and localUrl
+const PORT = process.env.PORT || 3000;
+const currentUrl = `${process.env.currencyUrl}:${PORT}`;
+
+//// db serevr from env
+const cloudDB = process.env.databaseUrl
+const localDB = process.env.MONGODB_URI
+
+const dataB = localDB || cloudDB
+
+app.get('/', (req, res) => {
+    res.send('hello there')
+});
+
+// // Import routes
+// const authRoute = require('./routes/authRoute');
+
+// // Routes middlewares
+// app.use('/api/auth', authRoute);
+
+// Database connection
+mongoose.connect(dataB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+/// db connection error
+db.on('error', (error) => {
+    console.error('Connection error:', error);
+});
+//// db connecrion status.Successful alert
+db.once('open', () => {
+    console.log('Connection to MongoDB successful!');
+});
+//// db connecrion status.failure  alert
+db.once('close', () => {
+    console.log('Connection to MongoDB disconnected.');
+});
+
+
+
+
+app.get('/', (req, res) => {
+    res.send(`i am here at ${currencyUrl} & Db${dataB}`)
+})
+
+
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`connected to PORT: ${PORT} using ${currentUrl}`)
+})
+
---
+++ models/User.js
@@ -0,0 +1,126 @@
+const mongoose = require('mongoose');
+
+//// User Schema
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  phoneNumber: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  
+  verifiedEmail: {
+    type: Boolean,
+    default: false,
+  },
+
+  verifiedPhone: {
+    type: Boolean,
+    default: false,
+  },
+
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'],
+  },
+
+  dob: {
+    type: Date,
+  },
+
+  role: {
+    type: String,
+    enum: ['isUser', 'isAdmin', 'isModerator'],
+    default: 'isUser',
+  },
+
+
+  placeOfOrigin: {
+    town: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+  },
+  resident: {
+    town: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+  },
+  
+  profilePhotos: [
+    {
+      url: {
+        type: String
+      },
+      description: {
+        type: String
+      },
+    },
+  ],
+ 
+
+
+
+  // createdBy field as an array of objects
+  createdBy: [
+    {
+      admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      fullnme: {
+        type: String
+      },
+      email: {
+        type: String
+      },
+      content: {
+        type: String
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+},
+  //////// TIME STAMPS -- CREATED AT AND UPDATED //////
+  { timestamps: true }
+);
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
