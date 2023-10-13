a2245543fdb96e5c9a1c04cac4e3b4289f70601d
donnwilzy <ask4godswill@gmail.com>
2023-10-13 02:34:12 +0100

    Regsiter User route adjuted

--- controllers/authContollers.js
+++ controllers/authContollers.js
@@ -25,72 +25,69 @@ const generateOTPCode = () => {
       let user = await User.findOne({ email });
   
       if (user) {
-        if (user.verified) {
-          // Remove the password field from the user object before sending the response
-          delete user.password;
-  
+        if (user.verifiedEmail) {
+          // User already exists and is verified
           return res.status(400).json({
             status: 'fail',
-            message: 'User already exists and is verified.',
-            user: user,
+            message: 'Account already exists and is verified. Kindly login.',
+          });
+        } else {
+          // User exists but is not verified
+          // Resend OTP for account verification
+          const otpCode = generateOTPCode();
+  
+          // Set the expiration time to 10 minutes from now
+          const expirationTime = new Date(Date.now() + 10 * 60 * 1000);
+  
+          // Save OTP code to the database
+          const otpCodeRecord = new OTPCode({
+            userId: user._id,
+            code: otpCode,
+            createdAt: Date.now(),
+            expiresAt: expirationTime,
+          });
+          await otpCodeRecord.save();
+  
+          // Prepare and send the email using the transporter and sendEmail function
+          const mailOptions = {
+            from: process.env.AUTH_EMAIL,
+            to: user.email,
+            subject: 'Verify Your Email',
+            html: `
+              <h1>Email Verification</h1>
+              <h3>Welcome ${fullname}, </h3>
+              <p>Please enter the verification code to continue.</p>
+              <h2><strong>${otpCode}</strong></h2>
+            `,
+          };
+  
+          await transporter.sendMail(mailOptions);
+  
+          return res.status(200).json({
+            status: 'success',
+            message: 'Account already registered, new OTP sent for verification.',
           });
         }
-  
-        // Resend OTP for account verification
-        const otpCode = generateOTPCode();
-  
-        // Set the expiration time to 10 minutes from now
-        const expirationTime = new Date(Date.now() + 10 * 60 * 1000);
-  
-        // Save OTP code to database
-        const otpCodeRecord = new OTPCode({
-          userId: user._id,
-          code: otpCode,
-          createdAt: Date.now(),
-          expiresAt: expirationTime,
-        });
-        await otpCodeRecord.save();
-  
-        // Prepare and send the email using the transporter and sendEmail function
-        const mailOptions = {
-          from: process.env.AUTH_EMAIL,
-          to: user.email,
-          subject: "Verify Your Email",
-          html: `
-            <h1>Email Verification</h1>
-            <h3>Welcome ${fullname}, </h3>
-            <p>Please enter the verification code to continue.</p>
-            <h2><strong>${otpCode}</strong></h2>
-          `,
-        };
-  
-        await transporter.sendMail(mailOptions);
-  
-        return res.status(200).json({
-          status: 'success',
-          message: 'Account already registered, new OTP sent for verification.',
-          user: user,
-        });
       }
   
       // If the user does not exist, create a new user and set their verified status to false
       const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of salt rounds
       const newUser = new User({
         fullname,
-       email,
+        email,
         password: hashedPassword,
         verifiedEmail: false,
       });
   
       const savedUser = await newUser.save();
   
-      // Send OTP code to user's email
+      // Send OTP code to the user's email
       const otpCode = generateOTPCode();
   
       // Set the expiration time to 10 minutes from now
       const expirationTime = new Date(Date.now() + 10 * 60 * 1000);
   
-      // Save OTP code to database
+      // Save OTP code to the database
       const otpCodeRecord = new OTPCode({
         userId: savedUser._id,
         code: otpCode,
@@ -103,11 +100,11 @@ const generateOTPCode = () => {
       const mailOptions = {
         from: process.env.AUTH_EMAIL,
         to: savedUser.email,
-        subject: "Verify Your Email",
+        subject: 'Verify Your Email',
         html: `
-        <h1>Email Verification</h1>
-        <p> Hello, ${fullname}, Welcome to Center Kick Community. Please enter the verification code to continue.</p>
-        <h3><strong>${otpCode}</strong></h3>
+          <h1>Email Verification</h1>
+          <p> Hello, ${fullname}, Welcome to Center Kick Community. Please enter the verification code to continue.</p>
+          <h3><strong>${otpCode}</strong></h3>
         `,
       };
   
@@ -116,7 +113,6 @@ const generateOTPCode = () => {
       return res.status(200).json({
         status: 'success',
         message: 'Sign up successful, OTP sent for verification.',
-        user: savedUser,
       });
     } catch (error) {
       console.error('Error while registering user:', error);
@@ -126,6 +122,7 @@ const generateOTPCode = () => {
       });
     }
   };
+  
 
 
   
